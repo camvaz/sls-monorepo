8a0cfaa96ba2a0920404521dae341ce22d0bbe8c
camvaz <camposvazquezv@gmail.com>
2020-12-28 00:20:04 -0600

    inicial

---
+++ services/auth-api/handler.ts
@@ -0,0 +1,17 @@
+import { Context, APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
+
+export async function hello(
+  event: APIGatewayEvent,
+  context: Context
+): Promise<APIGatewayProxyResult> {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Go Serverless v2.0! Your function executed successfully!",
+      context,
+      event,
+    }),
+  };
+}
+
+// export const handler = hello;
